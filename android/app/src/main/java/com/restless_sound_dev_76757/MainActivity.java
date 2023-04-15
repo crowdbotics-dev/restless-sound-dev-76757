9dfb255ec6402697545df7af3185b46045001397
Crowdbotics <engineering@crowdbotics.com>
2023-04-15 00:22:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/restless_sound_dev_76757/MainActivity.java
@@ -0,0 +1,15 @@
+package com.restless_sound_dev_76757;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "restless_sound_dev_76757";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-sound-dev-76757",
+  url: "https://restless-sound-dev-76757.botics.co",
+  api: "https://restless-sound-dev-76757.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
